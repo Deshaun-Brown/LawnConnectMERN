2f64eb574f1c0c4afb5457d1464b51d05571665b
Deshaun Brown <deshaunbrown933@yahoo.com>
2025-10-15 20:53:27 -0500

    Commit Summary

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-import express from 'express';
-const app = express();
-import debug from 'debug';
-import {ping} from './database.js';
-const debugIndex = debug('app:index');
-import cors from 'cors';
-const port = process.env.PORT || 8080;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 2025;
 
+app.use(express.urlencoded({extended: true}));//Middleware
+//Data from a form  
+//name=John&age=30
 
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.json());
-app.use(cors()); 
-app.use(express.static('frontend/dist'));
-app.use('/api/companies',(await import('./routes/api/company.js')).companyRouter);
-app.use('/api/jobs', (await import('./routes/api/job.js')).jobRouter);
-app.use('/api/users', (await import('./routes/api/users.js')).usersRouter);
+app.use(express.json());//Middleware
+//accept json to the req.body 0
+
+app.use(express.static('frontend/dist'));//Middleware
+
+app.get('/', (req, res) => {
+    //req.body.name
+    //req.body.age
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
-  debugIndex(`Example app listening on port http://localhost:${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
